90b432e78da88587e11e7990549fef76b674d433
Daniel Smith <jellymann@gmail.com>
2016-08-15 22:53:33 +0200

    Initial Commit

---
+++ Sour Lemon/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Sour Lemon
+//
+//  Created by Daniel Smith on 2016/08/15.
+//  Copyright © 2016 Sour Lemon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
