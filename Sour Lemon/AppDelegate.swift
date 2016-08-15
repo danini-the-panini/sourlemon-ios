16c6b1ad122ae6e4930ae370bc8273aaadb11460
Daniel Smith <jellymann@gmail.com>
2016-08-15 23:48:32 +0200

    feaet: configure special user agent

--- Sour Lemon/AppDelegate.swift
+++ Sour Lemon/AppDelegate.swift
@@ -1,11 +1,12 @@
 import UIKit
+import WebKit
 import Turbolinks
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
     var navigationController = UINavigationController()
-    var session = Session()
+    var session: Session!
     
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         window?.rootViewController = navigationController
@@ -14,6 +15,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
     
     func startApplication() {
+        let configuration = WKWebViewConfiguration()
+        configuration.applicationNameForUserAgent = "SourLemon/\(self.getAppVersion())";
+        session = Session(webViewConfiguration: configuration)
         session.delegate = self
         visit(NSURL(string: "http://localhost:3000")!)
     }
@@ -22,6 +26,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         let visitableViewController = VisitableViewController(URL: URL)
         navigationController.pushViewController(visitableViewController, animated: true)
         session.visit(visitableViewController)
+    }
+    
+    func getAppVersion() -> String {
+        let version = NSBundle.mainBundle().infoDictionary?["CFBundleShortVersionString"] as? String
+        
+        return version!
     }
 }
 
